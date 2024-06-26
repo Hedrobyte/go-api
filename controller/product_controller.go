0619a73a839ce767ede7badb1c3a67b0209bbb8b
hedrobyte <pedrohrmalves@gmail.com>
2024-06-26 14:45:40 -0300

    feat: Implement POST endpoint

--- controller/product_controller.go
+++ controller/product_controller.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"go-api/model"
 	"go-api/usecase"
 	"net/http"
 
@@ -24,5 +25,25 @@ func (p *ProductController) GetProducts(ctx *gin.Context) {
 		ctx.JSON(http.StatusInternalServerError, err)
 	}
 	ctx.JSON(http.StatusOK, products)
+
+}
+
+func (p *ProductController) CreateProduct(ctx *gin.Context) {
+
+	var product model.Product
+	err := ctx.BindJSON(&product)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, err)
+		return
+	}
+
+	insertedProduct, err := p.productUseCase.CreateProduct(product)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, err)
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, insertedProduct)
 
 }
